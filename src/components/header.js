ac1bd2d644b2a9ddeeb6a41c030cfec964d5fde9
Anna Bendahan <anna@blacksmith.studio>
2020-01-19 21:52:34 +0100

    home designed

--- src/components/header.js
+++ src/components/header.js
@@ -5,28 +5,14 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
+      display: `flex`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+    <h1>ALICE THONNIER</h1>
+    <div className="navs">
+      <Link>COLLECTIONS</Link>
+      <Link>ABOUT</Link>
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,199 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import logo from "../images/sitealice/12H50.png"
+import N2 from "../images/sitealice/N2.jpg"
+import BX7 from "../images/sitealice/BX7.jpg"
+import BX2 from "../images/sitealice/BX2.png"
+import BX3 from "../images/sitealice/BX3.jpg"
+import BX8 from "../images/sitealice/BX8.png"
+import HDP7 from "../images/sitealice/HDP7.jpg"
+import HDP1 from "../images/sitealice/HDP1.png"
+import HDP6 from "../images/sitealice/HDP6.png"
+import HDP3 from "../images/sitealice/HDP3.png"
+import backstage from "../images/sitealice/backstage.mp4"
+import Q1 from "../images/sitealice/Q1.png"
+import N3 from "../images/sitealice/N3.jpg"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends Component {
+  state = {
+    mounted: false,
+    p: 0,
+    a: 200,
+    c: 400,
+    v: -50,
+    l: -50,
+
+    step2: false,
+  }
+
+  componentDidMount() {
+    setInterval(
+      function() {
+        let p = this.state.p
+        let a = this.state.a
+        let c = this.state.c
+        let v = this.state.v
+        let l = this.state.l
+        this.setState({ p: p - 1, a: a - 1, c: c - 1, v: v + 1, l: l + 1 })
+      }.bind(this),
+      100
+    )
+    setTimeout(
+      function() {
+        this.setState({ step2: true })
+      }.bind(this),
+      100
+    )
+  }
+
+  render() {
+    let p = this.state.p
+    let a = this.state.a
+    let c = this.state.c
+    let v = this.state.v
+    let l = this.state.l
+    return (
+      <Layout>
+        <SEO title="Home" />
+        {/* <div className="logo">
+          <img src={logo} alt="" />
+        </div> */}
+
+        <div
+          className="all"
+          style={{
+            width: "100%",
+            height: "2000px",
+            transform: this.state.step2
+              ? "translateY(-2000px)"
+              : "translateY(100px)",
+            transition: "transform 100s ease-out",
+          }}
+        >
+          <img
+            style={{ position: "absolute", top: "200px", left: "15%" }}
+            src={N2}
+            className="pic"
+            alt=""
+          />
+          <br />
+
+          <img
+            style={{ position: "absolute", right: "5%" }}
+            src={BX7}
+            className="pic"
+            alt=""
+          />
+
+          <img
+            style={{
+              position: "absolute",
+              top: "400px",
+
+              left: `${l}px`,
+            }}
+            src={BX2}
+            className="pic"
+            alt=""
+          />
+
+          <img
+            style={{
+              position: "absolute",
+              left: "20%",
+
+              top: "1000px",
+            }}
+            src={HDP7}
+            className="pic"
+            alt=""
+          />
+
+          <img
+            style={{
+              position: "absolute",
+              top: "1300px",
+              left: `${a}px`,
+            }}
+            src={HDP3}
+            className="pic"
+            alt=""
+          />
+
+          <img
+            style={{
+              position: "absolute",
+              top: "700px",
+              right: `-2%`,
+            }}
+            src={Q1}
+            className="pic"
+            alt=""
+          />
+          <img
+            style={{
+              position: "absolute",
+              top: "850px",
+              right: `${l}px`,
+            }}
+            src={BX3}
+            className="pic"
+            alt=""
+          />
+          <img
+            style={{
+              position: "absolute",
+              top: "1500px",
+              right: `40%`,
+            }}
+            src={N3}
+            className="pic"
+            alt=""
+          />
+
+          <img
+            style={{
+              position: "absolute",
+              top: "1950px",
+
+              left: `${v}px`,
+            }}
+            src={BX8}
+            className="pic"
+            alt=""
+          />
+
+          <img
+            style={{
+              position: "absolute",
+              top: "2000px",
+
+              right: "-10px",
+            }}
+            src={HDP6}
+            className="pic"
+            alt=""
+          />
+
+          <img
+            style={{
+              position: "absolute",
+              top: "1800px",
+
+              right: "10%",
+            }}
+            src={HDP1}
+            className="pic"
+            alt=""
+          />
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
